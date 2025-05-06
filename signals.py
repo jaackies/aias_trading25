0e0215407918e1e2cd2ad3a0e17995c16a489a67
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2025-05-06 20:03:52 +0800

    kinda working

--- signals.py
+++ signals.py
@@ -1,28 +1,39 @@
 import numpy as np
 
 
-def pad(P, N):
-    # where P is your array of data points
-    # and N is the # data pt.
-    padding = -np.flip(P[1:N])
-    return np.append(padding, P)
+# def pad(P, N):
+#     # where P is your array of data points
+#     # and N is the # data pt.
+#     padding = -np.flip(P[1:N])
+#     return np.append(padding, P)
 
 
-def sma_filter(N):
+def sma_filter(N: int) -> np.ndarray:
     return np.ones(N) / N
 
 
-def lma_filter(N):
-    return (np.full(N, 2) / (N + 1)) * (1 - (np.arange(N) / N))
+def lma_filter(N: int) -> np.ndarray:
+    # return (np.full(N, 2) / (N + 1)) * (1 - (np.arange(N) / N))
+    # return (np.full(N, 2) - (np.arange(N) / N)) / (N + np.ones(N))
+    w = np.arange(1, N + 1)
+    return w / w.sum()
 
 
 def ema_filter(sf, N):
+    # w = sf * ((1 - sf) ** np.arange(N))
+    # return w / np.sum(w)  # Normalize the weights
+    # return np.full(N, sf) * np.power((np.ones(N) - np.full(N, sf)), np.arange(N))
     w = sf * ((1 - sf) ** np.arange(N))
-    return w / np.sum(w)  # Normalize the weights
+    return w / w.sum()
 
 
-def wma(P, N, kernel):
+def wma(P: np.ndarray, kernel: np.ndarray):
+    N = len(kernel)
+    padded_P = np.concat((np.flip(P[1:N]), P))
+    kernel /= kernel.sum()
+    return np.convolve(padded_P, np.flip(kernel), "valid")
+
     # P = array of data points
     # N = number of days
     # kernel = filter method called on N
-    return np.convolve(pad(P, N), kernel, "valid")
+    # return np.convolve(pad(P, N), kernel, "valid")
