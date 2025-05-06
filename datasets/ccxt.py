d08b7bd1b8362be20d6661d18e6407c9676aa1aa
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2025-05-06 20:56:59 +0800

    spring cleaning

---
+++ datasets/ccxt.py
@@ -0,0 +1,3 @@
+# Could maybe pull data from ccxt (https://github.com/ccxt/ccxt), for more data but this is not necessary for the project.
+# The same library would also allow you to actually trade but that might be another project in itself.
+# This project is more as a proof of concept and to learn about trading strategies and how to implement them.
