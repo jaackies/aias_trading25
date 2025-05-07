4bac95094d5cd002e64eed7a2af2cdc10bced8db
unknown <limeng5359@gmail.com>
2025-05-07 21:17:12 +0800

    Update again

--- run_training.py
+++ run_training.py
@@ -1,5 +1,6 @@
 from woa import whale_optimization
 from bot_training import bot_training
+from hill_climbing import hill_climbing
 
 # fitness_func: evaluation function;
 # dim: dimension of the solution. For example: for the solution (x, y), we need two parameters. so "dim = 2"
@@ -11,9 +12,12 @@ from bot_training import bot_training
 # bot_type = "complex" bounds bounds=[(1, 10), (1, 10), (1, 10), # w1, w2, w3 (2, 50), (2, 50), (2, 50), # d1, d2, d3 (0, 1), # sf # -- Low part --(1, 10), (1, 10), (1, 10),(2, 90), (2, 90), (2, 90),(0, 1)]
 # best, score= whale_optimization(fitness_func=bot_training, bot_type="sma", dim=2, bounds=[(1,10), (11,40)], num_agents= 10, max_iter= 100, integer_dims= [0,1])
 # best, score= whale_optimization(fitness_func=bot_training, bot_type="smaema", dim=3, bounds=[(1,10), (11,40), (0,1)], num_agents= 10, max_iter= 100, integer_dims= [0,1])
-best, score= whale_optimization(fitness_func=bot_training, bot_type="complex", dim=14, bounds=[(1, 10), (1, 10), (1, 10), (2, 50), (2, 50), (2, 50), (0, 1), (1, 10), (1, 10), (1, 10),(2, 90), (2, 90), (2, 90),(0, 1)], num_agents= 10, max_iter= 10, integer_dims= [3,4,5,10,11,12])
+# best, score= whale_optimization(fitness_func=bot_training, bot_type="complex", dim=14, bounds=[(1, 10), (1, 10), (1, 10), (2, 50), (2, 50), (2, 50), (0, 1), (1, 10), (1, 10), (1, 10),(2, 90), (2, 90), (2, 90),(0, 1)], num_agents= 10, max_iter= 10, integer_dims= [3,4,5,10,11,12])
+sma_optimals, cash_result = hill_climbing('sma',[(1,10),(11,40)])
 # low: (11, 40)， (51,100)
 # high: (1, 10), (5, 50)
 
-print(f"Best solution: {best}")
-print(f"Best score: {score}")
+# print(f"Best solution: {best}")
+# print(f"Best score: {score}")
+print(sma_optimals)
+print(cash_result)
